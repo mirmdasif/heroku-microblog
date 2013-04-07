7ed00d35fc832cf4e934311642c97de3c80a5477
sadasidha <sadasidha08@gmail.com>
2013-04-07 21:00:35 +0600

    added environment

--- web.js
+++ web.js
@@ -41,8 +41,24 @@ app.get('/',function (req,res){
   res.render('index',{title:'welcome'});
 });
 app.get('/environment',function (req,res){
-  res.send(process.env);
+  
+  var mongo = require('mongodb');
+
+  var mongoUri = process.env.MONGOLAB_URI || 
+    process.env.MONGOHQ_URL || 
+    'mongodb://localhost/mydb'; 
+
+  mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('mydocs', function(er, collection) {
+      collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+        res.send(rs);
+      });
+    });
+  });
+  
+  
 });
+
 
 
 app.get('/blog', function(req, res){
